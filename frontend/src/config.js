efc32cbb2b6ebb0ee82c8dbbee45c87bf61dbbc0
alok vaishnav <alokvaishnav545@fmail.com>
2026-01-01 23:09:01 +0530

    Fixed missing Core models and reset migrations

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // ✅ CONNECT TO YOUR AWS SERVER
-export const API_URL = "http://16.171.144.127:8000";
+export const API_URL = "http://16.171.144.127";
 
 // ℹ️ If you are testing on your laptop only, you can change it to:
 // export const API_URL = "http://127.0.0.1:8000";
