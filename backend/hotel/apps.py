fe619cd70a6efbfe74de44f0999ced2372872349
alok vaishnav <alokvaishnav545@fmail.com>
2026-01-02 19:01:10 +0530

    Fix backend settings app name to hotel

--- backend/hotel/apps.py
+++ backend/hotel/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class AtithiApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'atithi_api'
+    name = 'hotel'
     verbose_name = 'Atithi Hotel Management System'
